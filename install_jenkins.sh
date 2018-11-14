52a80bf46bdbc91ea827b68b2de34157a4fdf830
Ubuntu <ubuntu@askap-ci.novalocal>
2018-11-14 07:40:57 +0000

    jenkins installation recipe with Java 8 on Ubuntu

---
+++ install_jenkins.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install openjdk-8-jdk
+
+wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
+
+sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt-get update
+
+sudo apt-get install jenkins
